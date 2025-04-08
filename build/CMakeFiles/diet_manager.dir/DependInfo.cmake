5500293eef310f17368925553b4c713b18df1ce5
vishakkashyapk30 <vishakkashyapk300705@gmail.com>
2025-04-08 18:08:34 +0530

    fixed design doc

---
+++ build/CMakeFiles/diet_manager.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/cli.cpp" "CMakeFiles/diet_manager.dir/src/cli.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/cli.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/main.cpp" "CMakeFiles/diet_manager.dir/src/main.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/main.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/manager/food_database.cpp" "CMakeFiles/diet_manager.dir/src/manager/food_database.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/manager/food_database.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/manager/user_profile.cpp" "CMakeFiles/diet_manager.dir/src/manager/user_profile.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/manager/user_profile.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/models/food.cpp" "CMakeFiles/diet_manager.dir/src/models/food.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/models/food.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/models/log_entry.cpp" "CMakeFiles/diet_manager.dir/src/models/log_entry.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/models/log_entry.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/models/user.cpp" "CMakeFiles/diet_manager.dir/src/models/user.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/models/user.cpp.o.d"
+  "/home/oem/IIITH-II-II/DASS/diet_manager/src/utils/terminal_colors.cpp" "CMakeFiles/diet_manager.dir/src/utils/terminal_colors.cpp.o" "gcc" "CMakeFiles/diet_manager.dir/src/utils/terminal_colors.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
